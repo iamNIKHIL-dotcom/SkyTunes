f8d9df690a1b7f14414c12288ec0b9d4200f00b8
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-03-12 17:02:06 +0530

    basic route to gemini

--- server/index.js
+++ server/index.js
@@ -18,7 +18,16 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 
 app.post("/", async (req,res)=>{
-    
+    const city = req.body.city
+    const weather = req.body.weather
+
+    const prompt = `I am providing you weather condition and city name and trying to find a spotify song/playlist 
+    perfect for these weather conditions and region. I am searching for songs using this endpoint 
+    https://api.spotify.com/v1/search?q=genre:genre&type=track&limit=5, basically give me genre for these conditions, no other bs just one response with suitable genere. Focus on giving generes suitable to those regions like for punjab region any city of punjab lets say chandigarh it should be punjabi, for any haryanavi city it should be haryanavi etc etc.Return genre and market code(based on city and availaible for spotify) seperated by ',' . Weather Conditions: ${weather}  City: ${city}`;
+
+     const result = await model.generateContent(prompt);
+
+     console.log(result.response.text().split(","));
 
 })
 
